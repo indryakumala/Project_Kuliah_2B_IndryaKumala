e2255c599057758400ef6189d35b43e4f38eff0e
indryakumala <indryakumala472@gmail.com>
2023-10-16 12:33:02 +0700

    perubahan

--- laporan_5/bukutamu_add_form.php
+++ laporan_5/bukutamu_add_form.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simpan Buku Tamu</title>
 </head>
 <body>
     <h1>Simpan Buku Tamu MySQL</h1>
     <?php
-    $nama = $_POST["nama"];
+    $nama  = $_POST["nama"];
     $email = $_POST["email"];
     $komentar = $_POST["komentar"];
-    $conn = mysqli_connect ("localhost","root","","db_saya") or die ("koneksi gagal");
-        echo "Nama  : $nama <br>";
-        echo "Email  : $email <br>";
-        echo "Komentar  : $komentar <br>";
-        $hasil = mysqli_query($conn,"insert into bukutamu (nama, email, komentar values ('$nama','$email','$komentar)");
-        echo "Simpan Bukutamu berhasil dilakukan";
-        ?>
-        
-        
+    $conn = mysqli_connect ("localhost", "root", "", "db_saya") or die ("koneksi gagal");
+    echo "nama : $nama <br>";
+    echo "email : $email <br>";
+    echo "Komentar : $komentar <br>";
+    $hasil = mysqli_query($conn, "INSERT INTO bukutamu (nama,email,komentar) VALUES ('$nama','$email','$komentar')");
+    echo "Simpan bukutamu berhasil dilakukan";
+    ?>
 </body>
 </html>

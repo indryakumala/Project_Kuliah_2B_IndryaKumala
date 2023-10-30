1ca9e8de12f53cba270ac9845e5612c40c591afd
indryakumala <indryakumala472@gmail.com>
2023-10-30 20:43:31 +0700

    progres decafe

---
+++ decafe/customer.php
@@ -0,0 +1,15 @@
+<div class="col-lg-9 mt-2">
+        <div class="card">
+          <div class="card-header">
+            Customer
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Ini adalah bagian customer</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui incidunt soluta corrupti, repellendus
+              tempore eveniet quod vitae nisi? Blanditiis, nostrum laudantium. Excepturi et expedita exercitationem
+              reprehenderit corporis molestias doloremque accusamus?</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>

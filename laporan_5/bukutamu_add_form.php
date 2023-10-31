b6a9418244fc29e1915d0e94c012907922c440f6
indryakumala <indryakumala472@gmail.com>
2023-10-31 19:21:01 +0700

    laporan 5

--- laporan_5/bukutamu_add_form.php
+++ laporan_5/bukutamu_add_form.php
@@ -14,7 +14,7 @@
     $conn = mysqli_connect ("localhost", "root", "", "db_saya") or die ("koneksi gagal");
     echo "nama : $nama <br>";
     echo "email : $email <br>";
-    echo "Komentar : $komentar <br>";
+    echo "komentar : $komentar <br>";
     $hasil = mysqli_query($conn, "INSERT INTO bukutamu (nama,email,komentar) VALUES ('$nama','$email','$komentar')");
     echo "Simpan bukutamu berhasil dilakukan";
     ?>
